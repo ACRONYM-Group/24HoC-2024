8d63a42d0fafbe0e45342d445398d47543d42f0e
Ash <ashplasek@gmail.com>
2024-01-13 15:36:58 -0600

    Add more powerful thrust provider

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -9,7 +9,7 @@ var rot_vel = 0
 
 func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var delta_rotation = Input.get_vector("rotate_left", "rotate_right", "", "").x
+	var delta_rotation = Input.get_axis("rotate_left", "rotate_right")
 	
 	input_direction.y *= 2
 	
@@ -22,27 +22,35 @@ func get_input(delta):
 	else:
 		rot_vel *= 1.0 - (3 * delta)
 	
-	$Sprite2D/LeftThruster.reset()
-	$Sprite2D/RightThruster.reset()
+	$AnimatedSprite2D/LeftThruster.reset()
+	$AnimatedSprite2D/RightThruster.reset()
 		
 	if input_direction.x > 0:
-		$Sprite2D/LeftThruster.thruster_1 = true
+		$AnimatedSprite2D/LeftThruster.thruster_1 = true
 	elif input_direction.x < 0:
-		$Sprite2D/RightThruster.thruster_1 = true
+		$AnimatedSprite2D/RightThruster.thruster_1 = true
 		
 	if input_direction.y > 0:
-		$Sprite2D/LeftThruster.thruster_0 = true
-		$Sprite2D/RightThruster.thruster_2 = true
+		$AnimatedSprite2D/LeftThruster.thruster_0 = true
+		$AnimatedSprite2D/RightThruster.thruster_2 = true
 	elif input_direction.y < 0:
-		$Sprite2D/RightThruster.thruster_0 = true
-		$Sprite2D/LeftThruster.thruster_2 = true
+		$AnimatedSprite2D/RightThruster.thruster_0 = true
+		$AnimatedSprite2D/LeftThruster.thruster_2 = true
 		
 	if delta_rotation > 0:
-		$Sprite2D/LeftThruster.thruster_2 = true
-		$Sprite2D/RightThruster.thruster_2 = true
+		$AnimatedSprite2D/LeftThruster.thruster_2 = true
+		$AnimatedSprite2D/RightThruster.thruster_2 = true
 	elif delta_rotation < 0:
-		$Sprite2D/LeftThruster.thruster_0 = true
-		$Sprite2D/RightThruster.thruster_0 = true
+		$AnimatedSprite2D/LeftThruster.thruster_0 = true
+		$AnimatedSprite2D/RightThruster.thruster_0 = true
+		
+	if Input.is_action_just_pressed("thrust"):
+		$AnimatedSprite2D.play("thrusting")
+	if Input.is_action_just_released("thrust"):
+		$AnimatedSprite2D.play("default")
+	if Input.is_action_pressed("thrust"):
+		velocity += (Vector2(0, -1) * 5 * accel_rate).rotated(rotation) * delta
+		
 		
 
 func _physics_process(delta):
