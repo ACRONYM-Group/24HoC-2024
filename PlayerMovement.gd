f7e781f563955c1a8bcb1374765315b7a754e51f
Ash <ashplasek@gmail.com>
2024-01-13 14:22:51 -0600

    Add more pleasant movement

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -1,14 +1,24 @@
 extends CharacterBody2D
 
-@export var accel_rate = 25
-@export var rotation_rate = 0.05
+@export var accel_rate = 120
+@export var rotation_rate = 10
 
-func get_input():
+@export var max_rotation_rate = 180.0
+
+var rot_vel = 0
+
+func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	var delta_rotation = Input.get_vector("rotate_left", "rotate_right", "", "").x
-	velocity += (input_direction * accel_rate).rotated(rotation)
-	rotation += delta_rotation * rotation_rate
+	velocity += (input_direction * accel_rate).rotated(rotation) * delta
+	rotation += rot_vel * delta
+	rot_vel += delta_rotation * rotation_rate * delta
+	
+	if abs(rot_vel) >= max_rotation_rate:
+		rot_vel = rot_vel / (abs(rot_vel)/ max_rotation_rate)
+	else:
+		rot_vel *= 0.95
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	move_and_slide()
