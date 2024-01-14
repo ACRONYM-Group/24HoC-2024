63b0a152fa5d91bbd128b0d711dcb9d79d54e077
Jordan <jordanayan@live.com>
2024-01-14 13:10:28 -0600

    Asteroid Keep Out Zones

--- Asteroids.gd
+++ Asteroids.gd
@@ -4,13 +4,15 @@ extends Node
 @export var generated_area = 1000
 @export var last_generation_position = Vector2(0, 0)
 
+var keep_out_zones = [Vector3(0, 0, 1000)]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	generate_ring_tile(last_generation_position)
 	
 		
 func generate_ring_tile(player_position):
-	print("Generating new ring tile!")
+	keep_out_zones = [Vector3(0, 0, 1000)]
 	var asteroid_count = 0
 	remove_all_asteroids()
 	print(list_of_asteroids.size())
@@ -27,10 +29,18 @@ func generate_ring_tile(player_position):
 			var tile_y_offset = ((tile_y + ty) * (generated_area*2))
 			for i in range(5):
 				var location = Vector2i(rng.randi_range(-generated_area, generated_area) + tile_x_offset, rng.randi_range(-generated_area,generated_area) + tile_y_offset)
-				var new_asteroid = generate_asteroid(location, rng.seed+(asteroid_count*1100))
-				list_of_asteroids.append(new_asteroid)
-				add_child(new_asteroid)
-				asteroid_count += 1
+				var keep_out = false
+				for x in keep_out_zones:
+					if sqrt(pow(location.x - x.x, 2)+pow(location.y - x.y, 2)) < x.z:
+						keep_out = true
+						
+				if keep_out == false:
+					var new_asteroid = generate_asteroid(location, rng.seed+(asteroid_count*1100))
+					list_of_asteroids.append(new_asteroid)
+					add_child(new_asteroid)
+					asteroid_count += 1
+					
+					keep_out_zones.append(Vector3(location.x, location.y, 300))
 	
 	last_generation_position = player_position
 		
