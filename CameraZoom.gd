3b0adffeb161d8ced15318064ab4aad86ce23d52
Jordan <jordanayan@live.com>
2024-01-14 12:54:21 -0600

    New Build with fixed camera zoom

--- CameraZoom.gd
+++ CameraZoom.gd
@@ -10,5 +10,9 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_released("zoom_scroll_in"):
 		zoom *= 1.1
+		$"../../../background".scale.x = (1 / zoom.x)*3
+		$"../../../background".scale.y = (1 / zoom.y)*3
 	elif Input.is_action_just_released("zoom_scroll_out"):
 		zoom *= 0.9
+		$"../../../background".scale.x = (1 / zoom.x)*3
+		$"../../../background".scale.y = (1 / zoom.y)*3
