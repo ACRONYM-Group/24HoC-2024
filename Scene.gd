df6f62fae39bb826818257726d67e78358622529
Jordan <jordanayan@live.com>
2024-01-13 19:19:44 -0600

    Added Escape Menu

---
+++ Scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#print(str($CanvasLayer.visible) + " " + str(not $CanvasLayer.visible))
+	if (not $CanvasLayer.visible) == true:
+		#print("True")
+		$Player2.set_allow_input(true)
+	else:
+		$Player2.set_allow_input(false)
+		#print("False")
+	
