c7ae7a86306de44be7d76e74099e6f932ea467ee
Jordan <jordanayan@live.com>
2024-01-13 21:51:40 -0600

    Graphics

--- Scene.gd
+++ Scene.gd
@@ -9,6 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#print(str($CanvasLayer.visible) + " " + str(not $CanvasLayer.visible))
+	print($Player2.position)
+	$background.position = $Player2.position
 	if (not $CanvasLayer.visible) == true:
 		#print("True")
 		$Player2.set_allow_input(true)
