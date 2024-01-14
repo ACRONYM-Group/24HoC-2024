1ee8f15f454fa98ad2a260a94c363cf7a7b52a17
Jordan <jordanayan@live.com>
2024-01-13 23:29:04 -0600

    Added Minimap

--- Scene.gd
+++ Scene.gd
@@ -3,13 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#print(str($CanvasLayer.visible) + " " + str(not $CanvasLayer.visible))
-	print($Player2.positionwww)
 	$background.position = $Player2.position
 	if (not $CanvasLayer.visible) == true:
 		#print("True")
@@ -17,4 +16,6 @@ func _process(delta):
 	else:
 		$Player2.set_allow_input(false)
 		#print("False")
+		
+	$MiniMap/Minimap.render_map($Asteroids.list_of_asteroids, $Player2.position, $Asteroids.generated_area, $Player2.rotation)
 	
